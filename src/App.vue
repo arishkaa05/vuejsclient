d150ef80253287c092578b7f8f83c77a073b8055
pokaz <pokaz@LAPTOP-36VMD3L3>
2021-08-17 22:41:17 +0300

    Разработка клиента на Vue.JS

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-main>
+      <ChatPage/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ChatPage from './components/ChatPage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ChatPage
   }
 }
 </script>
---
+++ src/components/ChatPage.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-container>
+    <div id="example-1">
+      <button v-on:click="counter += 1">+1</button>
+      <p>The button above was pressed {{ counter }} time </p>
+    </div>
+  </v-container>
+
+</template>
+
+<script>
+export default {
+
+  data: () => {
+    return {
+      counter: 0,
+      username: '',
+      message: '',
+
+
+
+      messages: [],
+      intervalCtx: null,
+      lastMsgID: 0,
+    }
+
+  },
+}
+
+</script>
+
+<style scoped>
+
+</style>
